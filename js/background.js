29155dffd3d53df2ee132dad046a2c2126d3a740
Yohan <pyh95510@gmail.com>
2022-11-23 02:04:04 -0500

    random image generated

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = ["0.jpeg",
+"1.jpeg",
+"2.jpeg",
+"3.jpeg",
+"4.jpeg",
+"5.jpeg",
+"6.jpeg"];
+
+const imgChoose = images[Math.floor(Math.random()*(images.length))];
+console.log(imgChoose);
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${imgChoose}`;
+document.body.appendChild(bgImage);

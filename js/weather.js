25ed7cd939b66d48f7d07c9a4bf53f8dc815fb4e
Yohan <pyh95510@gmail.com>
2022-11-25 03:36:48 -0500

    change fahrenheit to celsius

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function onGeoSuccess(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metrics`
-    fetch(url);
+    fetch(url); //by using fetch, we call and bring the url.
 
 }
 
